210b7ae3af820703190038813f87f83f3fb964e8
stefan <skqu@iba.dk>
2023-09-17 09:59:31 +0200

    second part

--- Calculator.py
+++ Calculator.py
@@ -1,13 +1,22 @@
-#Du skal oprette en simpel lommeregner i Python, i første skridt skal den kun tage imod input og addere dem.
-# 1.	Pseudo kode: Første skridt er at overveje hvordan koden skal se ud, til dette bruger vi pseudo kode. Her skriver man koden, som hvis man vil forklare den. 
-# If user input then
-#   add it to new user input
-# output the result from the two numbers
-# 2.	Tag imod input: Næste skridt er at tage imod input fra brugeren. Til dette bruges funktionen input(). Hint: print dit input så du kan se om det virker 
-int_first = int(input("Type in the first number to be added: "))
-# 3.	Tag imod tal: Tredje skridt er at tage imod to tal fra brugeren. Disse skal bruges til vores addition. Hint: Begge tal skal bruges senere, så vi skal gemme dem i koden.
-int_second = int(input("Type in the second number to be added: "))  
-# 4.	Print Resultat: Læg nu de to tal sammen, og udskriv resultatet til brugeren. Hint: string + string eller integer + integer
+#Trin 2: Tilføj de andre operationer
+#Nu skal vi udvide vores lommeregner, således den kan bruge alle vores operationer. 
+#1.	Pseudo kode: Vi skal tilpasse vores pseudo kode, således vi har styr på alle vores operationer. Hint: vær opmærksom på hvordan man ser forskel fra de fire operationer.
+# take equation as input
+# if addition add number
+# else if subtraktion subtract numbers
+# else if multiplication then multiply numbers
+# else divide number 
+#2.	Tilføj de fire operationer: Lav nu de nødvendige ændringer i koden. Hint: tilføj en operation ad gangen, det er lettere at fejlsøge på to operationer i stedet for fire.
+int_first = int(input("Type in the first number and hit enter: "))
+str_op = input("Type in the operation and hit enter: ")
+int_second = int(input("Type in the second number and hit enter: ")) 
+#3.	Tilføj de fire operationer: Lav nu de nødvendige ændringer i koden. Hint: tilføj en operation ad gangen, det er lettere at fejlsøge på to operationer i stedet for fire. 
 if __name__ == "__main__":
-    str_result = str(int_first + int_second)
-    print(str_result)
+    if str_op == "+":
+        print(int_first + int_second)
+    elif str_op == "-":
+        print(int_first - int_second)
+    elif str_op == "*":
+        print(int_first * int_second)
+    else:
+        print(int_first/int_second)
--- README.md
+++ README.md
@@ -2,20 +2,22 @@
 
 Du skal oprette en simpel lommeregner i Python, der kan udføre de fire grundlæggende regneoperationer: addition, subtraktion, multiplikation og division. Derudover skal din lommeregner have fejlhåndtering indbygget for at undgå uønskede situationer, som f.eks. division med nul eller ugyldige operationer.
 
-### Trin 1: Simpel Lommeregner
+### Trin 2: Tilføj de andre operationer
 
-Du skal oprette en simpel lommeregner i Python. I første skridt skal den kun tage imod input og udføre addition.
+Nu skal vi udvide vores lommeregner, så den kan udføre alle vores operationer.
 
-1. **Pseudo kode:** Første skridt er at overveje, hvordan koden skal se ud. Til dette bruger vi pseudo kode. Her skriver man koden, som hvis man vil forklare den. F.eks. vil jeg gerne lave en kode, der vurderer, om et tal kan skrives med 4 bit. Her vil pseudo koden se således ud:
+1. **Pseudo kode:** Vi skal tilpasse vores pseudo kode, så vi har styr på alle vores operationer. Hint: Vær opmærksom på, hvordan man adskiller de fire operationer.
+
+    For eksempel kan pseudo koden se således ud for addition og subtraktion:
 
     ```
-    # Pseudo kode for vurdering af 4-bit tal
-    if tal er mindre end 16:
-        Tallet kan skrives med 4 bit
+    Hvis operationen er addition:
+        Udfør addition
+    Hvis operationen er subtraktion:
+        Udfør subtraktion
     ```
 
-2. **Tag imod input:** Næste skridt er at tage imod input fra brugeren. Til dette bruges funktionen `input()`. Hint: Print dit input, så du kan se, om det virker.
+2. **Tilføj de fire operationer:** Lav nu de nødvendige ændringer i koden for at tilføje de fire operationer. Hint: Tilføj en operation ad gangen. Det er lettere at fejlsøge med kun to operationer i stedet for fire.
 
-3. **Tag imod tal:** Tredje skridt er at tage imod to tal fra brugeren. Disse skal bruges til vores addition. Hint: Begge tal skal bruges senere, så vi skal gemme dem i koden.
+3. **Tilføj de fire operationer:** Lav nu de nødvendige ændringer i koden for at tilføje de fire operationer. Hint: Tilføj en operation ad gangen. Det er lettere at fejlsøge med kun to operationer i stedet for fire.
 
-4. **Print Resultat:** Læg nu de to tal sammen og udskriv resultatet til brugeren. Hint: `string + string` eller `integer + integer`.
