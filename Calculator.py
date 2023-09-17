2cc31be6289d8c26578033f9c691bfa5561d4970
stefan <skqu@iba.dk>
2023-09-17 11:23:08 +0200

    Part 4

--- Calculator.py
+++ Calculator.py
@@ -1,51 +1,82 @@
-# Trin 3: Fejlhåndtering
-# Nu skal vi ind i første fejlhåndtering – dette bliver uden exception dem tager vi bagefter. 
-# 1.	Identificere mulige fejl: Noter de fejl som i kan forstille jer en bruger laver, som vil være dumt på en lommeregner Hint: tænk både matematisk f.eks. kvadratroden af -1 men også rent brugen af systemet, gir det mening at subtrahere strenge. 
-# Potential error could be the following: 
-#   1. Division by zero - 5/0
-#   2. Calculate with wrong type - "2" + 2
-#   3. Unknown operation - 6%8
-#   4. Input format - 5++2
-# 2.	Pseudo kode: Vi skal tilpasse vores pseudo kode, så vi ved hvornår vi muligvis fejler. Hint: tænk løsninger på fejlene ind. 
-# if input is not numbers then
-#   tell user the input is not valid
-# else if the operation is unknown then
-#   tell user the operation is not valid
-# else if wrong input format then
-#   Type in valid input formai
-# else if division then
-#   if second number is 0 then
-#       tell user that division with zero is not valid
-# perfrom calculation and inform user
-# 3.	Tilføj den første fejlhåndtering: Lav nu de nødvendige ændringer i koden. Hint: test den godt igennem, måske virker den ikke som I forventer. 
-str_first = input("Type in the first number and hit enter: ")
-str_op = input("Type in the operation and hit enter: ")
-str_second = input("Type in the second number and hit enter: ")
-if __name__ == "__main__":
-    list_int = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-    list_operation = ["+", "/", "-", "*"]
-    list_input = [str_first, str_second]
-    int_index = 0
-    for str_input in list_input:
-        for i in str_input:
-            if i not in list_int:
-                exit("This calculator only takes numeric input")
-    if str_op not in list_operation:
-        exit("Not valid operation")
+# ExitExceoption
+# Heritage: exception
+class ExitException(Exception):
+    pass
 
-    int_first = int(str_first)
-    int_second = int(str_second)
 
-    # 4.	Tilføj resten: Når I har styr på den første så tilføj de resterende. Hint: Ligesom med operationer, sikre jer at den første virker før i går videre.  
+# calculator function
+# input :   int_first, integer, the first number for the calculator
+#           str_op, string, the operation for the calculator
+#           int_second, integer, the second number for the calculator
+# output : result from the calculator, NaN for errors
+# exception :  division by zero
+def calculator(int_first=0, str_op="+", int_second=0):
+    str_rtn = "" 
     if str_op == "+":
-        print(int_first + int_second)
+        str_rtn =  str(int_first + int_second)
     elif str_op == "-":
-        print(int_first - int_second)
+        str_rtn =  str(int_first - int_second)
     elif str_op == "*":
-        print(int_first * int_second)
-    elif int_second == 0:
-        print("Division by zero is not permitted")
+        str_rtn = str(int_first * int_second)
     elif str_op == "/":
-        print(int_first/int_second)
-    else:
-        print("Cannot calculate - an unexpected error happen")
+        try:
+            str_rtn = str(int_first / int_second)
+        except ZeroDivisionError:
+            print("Kan ikke divider med nul")
+            str_rtn = "NaN"    
+    return str_rtn
+
+
+# get_input
+# input: str_input, string, the original input from terminal
+# output:   int_op, integer, the index of the operation
+#           str_input, string, the input string from terminal
+# exception ExitException
+def get_input(str_input=""):
+    list_op = ["-", "+", "/", "*"]
+    int_next = 0
+    int_op = str_input.find(list_op[int_next])
+    while int_op == -1:
+        try:
+            int_op = str_input.find(list_op[int_next])
+            int_next += 1
+        except IndexError:
+            int_next = 0
+            print("Ikke Valid operation!!")
+            str_input = input("Hvilket regne stykke skal jeg lave? ")
+            if str_input.lower() == "exit":
+                raise ExitException("Lommeregner afslutter nu - tak for denne gang")
+    return int_op, str_input
+
+
+# main function
+# input :   str_input, string, original input string
+# output : 
+# exception :  ExitException
+def main(str_input):
+    if str_input.lower() == "exit":
+        raise ExitException("Lommeregner afslutter nu - tak for denne gang")
+    int_op, str_input = get_input(str_input=str_input)
+    str_op = str_input[int_op].strip()
+    try:
+        int_first = int(str_input[:int_op].strip())
+        int_second = int(str_input[int_op + 1:].strip())
+        str_result = calculator(int_first=int_first, str_op=str_op, int_second=int_second)
+        print("Resulatet af " + str_input + " = " + str_result)
+    except ValueError:
+        print("Jeg tager kun imod tal!!!")
+
+    str_input = input("Hvilket regne stykke skal jeg lave? ")
+    main(str_input)
+
+
+if __name__ == "__main__":
+    str_input = ""
+    print("Velkommen til lommeregner")
+    print("Skriv exit, hvis du vil afslutte.")
+
+    str_input = input("Hvilket regne stykke skal jeg lave? ")
+    try: 
+        main(str_input)
+    except ExitException as e:
+        print(e)
--- README.md
+++ README.md
@@ -2,16 +2,8 @@
 
 Du skal oprette en simpel lommeregner i Python, der kan udføre de fire grundlæggende regneoperationer: addition, subtraktion, multiplikation og division. Derudover skal din lommeregner have fejlhåndtering indbygget for at undgå uønskede situationer, som f.eks. division med nul eller ugyldige operationer.
 
-### Trin 3: Fejlhåndtering
+### Trin 4: Exception
 
-Nu skal vi begynde at implementere fejlhåndtering i vores lommeregner – dette trin vil omfatte basal fejlhåndtering uden brug af undtagelser.
+Nu skal vi tilføje vores try – exception blocks. 
 
-1. **Identificere mulige fejl:** Noter de fejl, som I kan forestille jer, at en bruger laver, som ville være uhensigtsmæssige på en lommeregner. Hint: Tænk både matematisk, f.eks. kvadratroden af -1, og også brugen af systemet. Er det meningsfuldt at trække strenge fra hinanden?
-
-2. **Pseudo kode:** Tilpas vores pseudo kode, så vi ved, hvornår vi potentielt kan støde på fejl. Hint: Tænk på løsninger til at håndtere disse fejl.
-
-3. **Tilføj den første fejlhåndtering:** Lav nu de nødvendige ændringer i koden for at håndtere den første identificerede fejl. Hint: Test grundigt, da det muligvis ikke fungerer som forventet.
-
-4. **Tilføj resten:** Når I har styr på den første fejlhåndtering, tilføj de resterende fejlhåndteringer. Hint: Ligesom med operationer, sørg for at den første fejlhåndtering fungerer, før I går videre til de næste.
-
-
+1. **Lav jeres egen steps herfra:** Som en del af denne opgave skal i definere jeres egen steps for at nå frem til et resultat. Hint: Tænk i bider, nu mindre des bedre.
